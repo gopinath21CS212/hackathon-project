ed976bfb42d8ef2dde0e5b7854a41c971d3ecf33
Gopinath <vipgopi99@gmail.com>
2026-05-05 13:49:01 +0530

    initial push

---
+++ src/test/java/org/districtappautomation/test/gowtham/TC015_MoviesBasedOnLocation.java
@@ -0,0 +1,25 @@
+package org.districtappautomation.test.gowtham;
+import org.districtappautomation.test.baseclass.BaseClass;
+
+import org.districtappautomation.test.pages.HomePage;
+import org.districtappautomation.test.pages.MoviePage;
+import org.districtappautomation.test.utility.LoggerUtil;
+import org.testng.annotations.Test;
+
+public class TC015_MoviesBasedOnLocation extends BaseClass {
+
+    @Test
+    public void extractMoviesBasedOnLocation() throws InterruptedException {
+        MoviePage moviesPage = new MoviePage(driver);
+        HomePage homePage = new HomePage(driver);
+        LoggerUtil.info("Successfully Launched the Application");
+        moviesPage.clickMovies();
+        homePage.changeLocationAndValidate("Chennai");
+        softAssert.assertTrue(
+                moviesPage.printDisplayeFilterMovieNames(),
+                "FAIL: No movies were found in this Location!"
+        );
+        LoggerUtil.info("Extracted Movies From Location Test Case Execution Completed");
+
+    }
+}
